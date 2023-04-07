278e3fb7a5b0bb3a0d859bdae78ff46ba94aa193
androidblog <androidblog@yandex.ru>
2023-04-07 09:54:18 +0400

    create global font styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,10 +8,9 @@ const inter = Inter({ weight: ["400", "700", "800"], subsets: ["latin"] });
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
-      <main 
-className={inter.className}>
+      <main className={inter.className}>
         <Component {...pageProps} />
-        </main>
+      </main>
     </Layout>
   );
 }
